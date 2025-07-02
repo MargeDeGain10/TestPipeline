4afb13f9201080d29fb403499ebe596438550b14
GUEYE <mouhamed.gueye@capgemini.com>
2025-07-02 10:53:49 +0200

    feat: Add CI workflow for quality checks

---
+++ tests/GreeterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Greeter;
+
+class GreeterTest extends TestCase
+{
+    public function testGreet()
+    {
+        $greeter = new Greeter();
+        $this->assertEquals('Hello from src!', $greeter->greet());
+    }
+}
